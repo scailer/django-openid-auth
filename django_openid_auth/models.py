2c531d1f0951c8a85de668d85072d2bec6c21b19
Vlasov Dmitriy <scailer@veles.biz>
2014-04-01 10:20:44 +0600

    model fix

--- django_openid_auth/models.py
+++ django_openid_auth/models.py
@@ -28,14 +28,13 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 import django
+from django.db import models
 
 if django.VERSION[1] < 5:
     from django.contrib.auth.models import User
 else:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-from django.db import models
+    from django.conf import settings
+    User = settings.AUTH_USER_MODEL
 
 
 class Nonce(models.Model):
